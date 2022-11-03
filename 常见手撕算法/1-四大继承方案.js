322e75fa7036816a703bfdfbec17eda55f9aeab8
shyriizhou <shyriizhou@tencent.com>
2022-11-03 17:37:05 +0800

    feat: 增加原型继承

--- 常见手撕算法/1-四大继承方案.js
+++ 常见手撕算法/1-四大继承方案.js
@@ -1,7 +1,26 @@
 // 1.原型继承
+// 在 JavaScript 中，对象有一个特殊的隐藏属性 [[Prototype]]，它要么为 null，要么就是对另一个对象的引用。该对象被称为“原型”
+// 当我们从 object 中读取一个缺失的属性时，JavaScript 会自动从原型中获取该属性。在编程中，这被称为“原型继承”。
+// __proto__ 是 [[Prototype]] 的因历史原因而留下来的 getter/setter
+// 现代编程语言建议我们应该使用函数 Object.getPrototypeOf/Object.setPrototypeOf 来取代 __proto__ 去 get/set 原型。
 // 并不会把父类的方法克隆一份给子类，而是建立父类与子类之间的原型链查找机制
-// 重定向子类的原型后，默认找不到原本的constructer属性
-// 子类或者子类的实例 可以基于原型链肆意的修改父类的属性和方法 对父类造成不必要的破坏
-// 会把父类中私有的属性和方法作为子类的共有属性继承过来
-// 父类中不管共有还是私有 都会变成子类的公有
+// 写/删除操作直接在对象上进行，它们不使用原型
+// 如果我们调用 obj.method()，而且 method 是从原型中获取的，this 仍然会引用 obj。因此，方法始终与当前对象一起使用，即使方法是继承的。
 
+let animal = {
+    eat() {
+        this.full = true;
+    },
+    walk() {
+        console.log('animal walk');
+    }
+}
+
+let rabbit = {
+    jumps: true,
+    __proto__: animal,
+}
+
+rabbit.walk();
+rabbit.eat();
+console.log(rabbit.full, animal.full);
